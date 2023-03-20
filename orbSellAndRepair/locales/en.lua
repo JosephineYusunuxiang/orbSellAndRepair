59ca3402257820125d0e7b57349c3ad79ac858ca
JosephineYusunuxiang <128058341+JosephineYusunuxiang@users.noreply.github.com>
2023-03-20 14:29:16 +0800

    Add files via upload

---
+++ orbSellAndRepair/locales/en.lua
@@ -0,0 +1,53 @@
+local _, L = ...; -- Let's use the private table passed to every .lua file to store our locale
+
+L.SELL_GREY = "售卖垃圾: "
+
+L.REPAIR_REPUT = "修理失败: 声望不足"
+L.REPAIR_MONEY = "修理失败: 没有足够的金币"
+L.REPAIR_OK = "修理成功，花费: "
+L.BALANCE_P = "|cff00ff00本次交易累计收入:|r "
+L.BALANCE_N = "|cffff0000本次交易累计支出:|r "
+
+L.URL_TEXT = "Ctrl-C复制URI"
+L.URL_BTN1 = "确定"
+L.URL_BTN2 = "取消"
+
+L.REPUTATIONS = {
+    "仇恨",
+    "敌对",
+    "冷淡",
+    "中立",
+    "友善",
+    "尊敬",
+    "崇敬",
+    "崇拜",
+}
+
+L.AutoRepairBtn = {
+    text = "自动修理",
+}
+
+L.UseGuildRepairBtn = {
+    text = "启用工会修理",
+}
+
+L.ReputSlider = {
+    text = "自动修理的最低声望",
+    tooltip = "自动修理的最低声望",
+}
+
+L.VendorGreysBtn = {
+    text = "自动售卖灰色物品"
+}
+
+L.VendorWhitesBtn = {
+    text = "自动售卖白色武器和护甲"
+}
+
+L.NameplateColorBtn = {
+    text = "Class nameplate color"
+}
+
+L.ClassIconPortraitBtn = {
+    text = "Class portrait (need /reload)"
+}
